76b51983efe804768c1eb3549766e6d1cee52c13
VinayRana307 <vinay6365355@gmail.com>
2020-09-04 21:07:25 +0530

    Updates

---
+++ precache-manifest.9ad1bc430426a67be53fb49de791c13a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e3623f65cba906fed61405e40dd746",
+    "url": "/carbooking/index.html"
+  },
+  {
+    "revision": "d44dc6844b16330e77ef",
+    "url": "/carbooking/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4733de21d3dcc60832e6",
+    "url": "/carbooking/static/js/2.8ad6cb9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/carbooking/static/js/2.8ad6cb9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d44dc6844b16330e77ef",
+    "url": "/carbooking/static/js/main.0b961cf4.chunk.js"
+  },
+  {
+    "revision": "ac92937f3a3d45c46a46",
+    "url": "/carbooking/static/js/runtime-main.d15861a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/carbooking/static/media/logo.5d5d9eef.svg"
+  }
+]);
